b49c9794f25aea23742e982ccc8abcc1df1e1a3e
Debora Santos <debora_santos@outlook.com.br>
2021-11-21 13:17:22 -0300

    modificado sistema para edição e cadastro de produtos ficaram do lado do browser

--- app.js
+++ app.js
@@ -25,10 +25,24 @@ app.get('/produtos', (req, res) => {
 })
 
 app.get('/cadastro', (req, res) => {
-    res.render('cadastro', {bairro:req.query.bairro})
+    let body = {bairro:req.query.bairro};
+    if(req.query.id){
+        body.id = req.query.id;
+        let url = `http://localhost:${databasePort}/produtos/${req.query.id}`;
+        console.log('req:', req.query); 
+        request(url, function (error, response, resBody) {
+            console.error('error:', error);
+            console.log('statusCode:', response && response.statusCode); 
+            console.log('body:', resBody);
+            body = JSON.parse(resBody);
+            res.render('cadastro', body);
+        });
+    }else
+        res.render('cadastro', body)
 })
 
 app.post('/cadastrar', (req, res) => {
+    console.log("cadastrando produto");
     console.log(req.body);
     let url = `http://localhost:${databasePort}/produtos`;
     request({
@@ -45,6 +59,27 @@ app.post('/cadastrar', (req, res) => {
                 console.log(response)
             }         
             
+            res.redirect('/');
+    });
+})
+
+app.get("/apagar", (req, res) => {
+    console.log("apagando produto");
+    console.log(req.query.id);
+    let url = `http://localhost:${databasePort}/produtos/${req.query.id}`;
+    request({
+        method:'delete',
+        headers: {'content-type' : 'application/json'},
+        url: url,
+        json: req.body,
+        
+        }, function(error, response, body){     
+            if(error){
+                console.log(error)
+            }
+            if(response){
+                console.log(response)
+            }         
             res.redirect('/');
     });
 })
