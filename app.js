c313355d5001a706db90f7849dbbf3da68b4dbdb
Debora Santos <debora_santos@outlook.com.br>
2021-11-21 02:14:58 -0300

    adicionada listagem e cadastro

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const request = require('request');
 
+
 const app = express();
 const port = 3000;
 const databasePort = 3004;
@@ -8,6 +9,8 @@ const databasePort = 3004;
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port,() => {
     console.log(`Servidor rodando na porta ${port}`);
@@ -17,20 +20,41 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/bairros', (req, res) => {
-    res.render('bairros')
+app.get('/produtos', (req, res) => {
+    res.render('produtos')
 })
 
 app.get('/cadastro', (req, res) => {
     res.render('cadastro')
 })
 
+app.post('/cadastrar', (req, res) => {
+    console.log(req.body);
+    let url = `http://localhost:${databasePort}/produtos`;
+    request({
+        method:'post',
+        headers: {'content-type' : 'application/json'},
+        url: url,
+        json: req.body,
+        
+        }, function(error, response, body){     
+            if(error){
+                console.log(error)
+            }
+            if(response){
+                console.log(response)
+            }         
+            
+            res.redirect('/');
+    });
+})
+
 app.get('/lista_produtos', (req, res) => {
     let url = `http://localhost:${databasePort}/produtos`;
     request(url, function (error, response, body) {
-        console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        console.error('error:', error);
+        console.log('statusCode:', response && response.statusCode); 
+        console.log('body:', body); 
         res.render('produtos', {produtos:body})
     });    
 })
